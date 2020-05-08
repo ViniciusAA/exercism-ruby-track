f474daea20d478d1f2a36e5e954ee5593658a130
Vinícius de Araújo Aquino <vini89go@gmail.com>
2020-05-08 01:19:32 -0300

    Create darts.rb

---
+++ Extra exercises/darts.rb
@@ -0,0 +1,15 @@
+class Darts
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def score
+    case Math.hypot(@x, @y)
+      when 0..1 then 10
+      when 1..5 then 5
+      when 5..10 then 1
+      else 0
+    end
+  end
+end
