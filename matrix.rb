0f0ac7476edcb5790b593ba5cdc8272875493da1
Vinícius de Araújo Aquino <vini89go@gmail.com>
2020-05-14 02:43:08 -0300

    Create matrix.rb

---
+++ matrix.rb
@@ -0,0 +1,13 @@
+class Matrix
+  def initialize(string)
+    @string = string
+  end
+
+  def rows
+    @string.split("\n").map{ |row| row.split(" ").map(&:to_i) }
+  end
+
+  def columns
+    rows.transpose
+  end
+end
